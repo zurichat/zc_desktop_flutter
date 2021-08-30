fcb52a0b1cd40fbdb3b0175cb14a629765c3210e
Stephen Obotu <osobotu@gmail.com>
2021-08-30 23:18:32 +0100

    completed package for app wide UI

---
+++ packages/zcdesk_ui/lib/zcdesk_ui.dart
@@ -0,0 +1,10 @@
+library zcdesk_ui;
+
+// widgets exports
+export 'src/widgets/zcdesk_text.dart';
+
+// colors export
+export 'src/shared/app_colors.dart';
+
+// shared exports
+export 'src/shared/ui_helpers.dart';
---
+++ packages/zcdesk_ui/test/zcdesk_ui_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:zcdesk_ui/zcdesk_ui.dart';
+
+void main() {}
