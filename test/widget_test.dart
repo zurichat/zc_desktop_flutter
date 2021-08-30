0c33da3ed88f5733c7749062ef8c5e9df9e915ea
toxic-bishop <okanlawon.daniel@yahoo.com>
2021-08-30 05:35:46 +0100

    add test folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,13 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zc_desktop_flutter/ui/views/home/home_view.dart';
 
 void main() {
-  //All test functions go inside here
+
+  testWidgets('Testing the homeview', (widgetTester)  async {
+    // Build our app and trigger a frame.
+    await widgetTester.pumpWidget(HomeView());
+  });
 }
