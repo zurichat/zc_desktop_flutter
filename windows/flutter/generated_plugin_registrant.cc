fed43a5572988f74ca631d774a3d73ab007fd807
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-09-01 20:26:12 +0100

    added bitsdojo package

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -2,8 +2,13 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
+#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  BitsdojoWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
 }
