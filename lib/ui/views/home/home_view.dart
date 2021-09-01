b0305c3981421a4893b786ee8733f32418cdf2bf
Adevikki <vadeala@gmail.com>
2021-09-01 20:42:00 +0100

    sign in form

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:zc_desktop_flutter/ui/login/login_view.dart';
 
 import 'home_viewmodel.dart';
 
@@ -9,30 +10,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>.reactive(
-      builder: (context, model, child) => Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 3,
-                child: Image(
-                  image: AssetImage(model.logoUrl),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Text(model.title),
-              ),
-              Expanded(
-                flex: 1,
-                child: Text(model.testString),
-              )
-            ],
-          ),
-        ),
-      ),
+      builder: (context, model, child) => Scaffold(body: LoginView()),
       viewModelBuilder: () => HomeViewModel(),
     );
   }
--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,16 +1,13 @@
 import 'package:stacked/stacked.dart';
-
-import 'package:zc_desktop_flutter/app/app.logger.dart';
-
 import 'package:zc_desktop_flutter/app/app.locator.dart';
+import 'package:zc_desktop_flutter/app/app.logger.dart';
 import 'package:zc_desktop_flutter/services/local_storage_service.dart';
 import 'package:zc_desktop_flutter/ui/views/startup/startup_viewmodel.dart';
-
 
 class HomeViewModel extends BaseViewModel {
   final log = getLogger("HomeViewwModel");
 
-  String _logoUrl = 'assets/images/zc_logo.png';
+  String _logoUrl = 'assets/images/zc_logo2.png';
   double _logoWidth = 10.0;
   double _logoHeight = 10.0;
   String _title =
