860d84794e0d86997a321b797ed8f41b51514df3
DELL <tanzebright@gmail.com>
2021-09-02 07:21:34 +0100

    1. Add background image 2. Add text

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:zc_desktop_flutter/app/app.locator.dart';
@@ -16,9 +17,18 @@ class StartUpViewModel extends BaseViewModel {
   // TODO: 1, This is to test that the local storage service is working properly. Remove after review
   final _storageService = locator<LocalStorageService>();
 
-
   String _appName = 'Zuri Chat';
   String get appName => _appName;
+
+  final String _circularImage = 'assets/images/circularImage.png';
+  String get circularImage => _circularImage;
+
+  String _title = 'Unlimited Team Collaboration Platform';
+  String _subTitle =
+      'Unlimited team collaboration platform with emphasis on team building,fun, team manageent and above all, project management.';
+
+  String get title => _title;
+  String get subTitle => _subTitle;
 
   void goToHome() {
     // TODO: 3, This is part of the local storage test. Remove this as well
