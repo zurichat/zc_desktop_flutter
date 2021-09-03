b10d6e18379cd4c48c783f481fefc1e4990454a7
Okanlawon Jamiu <mistaglobe@gmail.com>
2021-09-03 17:35:52 +0100

    Export added files

--- packages/zcdesk_ui/lib/zcdesk_ui.dart
+++ packages/zcdesk_ui/lib/zcdesk_ui.dart
@@ -5,10 +5,12 @@ export 'src/widgets/zcdesk_text.dart';
 export 'src/widgets/zcdesk_input_field.dart';
 export 'src/widgets/zcdesk_auth_btn.dart';
 export 'src/widgets/zc_desk_send_message_field.dart';
+export 'src/widgets/zc_desk_button.dart';
+export 'src/widgets/zc_desk_appbar.dart';
 
 // colors export
 export 'src/shared/app_colors.dart';
 
 // shared exports
 export 'src/shared/ui_helpers.dart';
-
+export 'src/shared/styles.dart';
