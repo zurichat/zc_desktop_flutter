b778b4449048ae063e5b09f61950b792c79bd60b
Perpety <berniceanne3@gmail.com>
2021-09-03 01:34:04 +0000

    Signup Page

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/berniceannie/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/berniceannie/Documents/Projects/Flutter/zc_desktop_flutter" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/berniceannie/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/berniceannie/Documents/Projects/Flutter/zc_desktop_flutter"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/berniceannie/Documents/Projects/Flutter/zc_desktop_flutter/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/berniceannie/Documents/Projects/Flutter/zc_desktop_flutter/lib/main.dart"
+)
