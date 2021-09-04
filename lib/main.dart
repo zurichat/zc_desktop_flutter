bc522dc145456e4dd800593eb391f3e267242d32
nnaeyes6 <nnaeyesworld@gmail.com>
2021-09-04 17:20:48 +0100

    'page-layout-desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_it/get_it.dart';
 
 import 'package:stacked_services/stacked_services.dart';
 import 'package:zc_desktop_flutter/app/app.locator.dart';
-import 'package:get_it/get_it.dart';
 
 import 'app/app.router.dart';
 
 void main() async {
   GetIt.instance;
-  await setupLocator();
-  runApp(ZcDesktop());
+  setupLocator();
+  runApp(const ZcDesktop());
 }
 
 class ZcDesktop extends StatelessWidget {
+  const ZcDesktop({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Zuri Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    return  ScreenUtilInit(
+      builder: () => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Zuri Chat',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+
+        navigatorKey: StackedService.navigatorKey,
+        onGenerateRoute: StackedRouter().onGenerateRoute,
       ),
-      navigatorKey: StackedService.navigatorKey,
-      onGenerateRoute: StackedRouter().onGenerateRoute,
+      designSize: const Size(375, 667),
     );
   }
 }
--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,39 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
+import 'package:zc_desktop_flutter/custom_widgets/home_view_widget.dart';
 
-import 'home_viewmodel.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<HomeViewModel>.reactive(
-      builder: (context, model, child) => Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 3,
-                child: Image(
-                  image: AssetImage(model.logoUrl),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Text(model.title),
-              ),
-              Expanded(
-                flex: 1,
-                child: Text(model.testString),
-              )
-            ],
-          ),
-        ),
-      ),
-      viewModelBuilder: () => HomeViewModel(),
-    );
+    return HomeViewWidget();
   }
 }
+
+
