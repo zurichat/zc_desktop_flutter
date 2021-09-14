c2eb600fdaeb74b55dab76a472cd3cf6d11c142a
ndungudedan <dnkibere@gmail.com>
2021-09-14 12:44:49 +0300

    start_dm_conversation_with_user

--- packages/zcdesk_ui/lib/src/shared/svg_assets_paths.dart
+++
@@ -1,5 +0,0 @@
-class SVGAssetPaths {
-  static final channelsListIcon = "assets/icons/hash_icon.svg";
-  static final dropDownOpenIcon = "assets/icons/drop_down_open.svg";
-  static final dropDownClosedIcon = "assets/icons/drop_down_closed.svg";
-}
