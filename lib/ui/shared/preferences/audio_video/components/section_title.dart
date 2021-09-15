b9bd486c09eb63242edf5c29750fb1492dc8034e
Abdul Azeez Olapade <olapadeabdulazeez1@gmail.com>
2021-09-15 13:01:21 +0100

    Audio and Video Preferences

---
+++ lib/ui/shared/preferences/audio_video/components/section_title.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../const_app_colors.dart';
+import '../../../const_text_styles.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: bodyStyle.copyWith(color: bodyColor, fontWeight: FontWeight.w700),
+    );
+  }
+}
