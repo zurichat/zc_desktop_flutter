f1ccb6c331d2850f7610f0f9d97122e897c47e55
toxic-bishop <okanlawon.daniel@yahoo.com>
2021-09-18 19:50:22 +0100

    update signup with confirmation

---
+++ lib/app/app.dart
@@ -0,0 +1,38 @@
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:zc_desktop_flutter/services/api/api_service.dart';
+import 'package:zc_desktop_flutter/services/authentication/auth_service.dart';
+import 'package:zc_desktop_flutter/services/local_storage/local_storage_service.dart';
+import 'package:zc_desktop_flutter/ui/auth/check_email_oage/check_email_view.dart';
+import 'package:zc_desktop_flutter/ui/auth/forgot_password_page/forgot_password_view.dart';
+import 'package:zc_desktop_flutter/ui/auth/login_page/login_view.dart';
+import 'package:zc_desktop_flutter/ui/auth/sign_up_page/sign_up_view.dart';
+import 'package:zc_desktop_flutter/ui/auth/success_page/success_view.dart';
+import 'package:zc_desktop_flutter/ui/main/home_page/home_view.dart';
+import 'package:zc_desktop_flutter/ui/startup_page/startup_view.dart';
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView, ),
+    MaterialRoute(page: LoginView, initial: true),
+    MaterialRoute(page: StartUpView),
+    MaterialRoute(page: SignUpView),
+    MaterialRoute(page: ForgotPasswordView),
+    
+    MaterialRoute(page: CheckEmailView),
+    MaterialRoute(page: SuccessView),
+    
+  ],
+  dependencies: [
+    Presolve(
+      classType: LocalStorageService,
+      presolveUsing: LocalStorageService.getInstance,
+    ),
+    LazySingleton(classType: NavigationService),
+    LazySingleton(classType: AuthService),
+    LazySingleton(classType: ApiService),
+
+  ],
+  logger: StackedLogger(),
+)
+class AppSetup {}
