16da460553a927e41e743e08f1908e5887877227
Chiemela Uzoma <melasin4@gmail.com>
2021-09-19 03:14:31 +0100

    Refactored and added new text styles in Accessibility and Advanced

--- lib/ui/shared/const_widgets.dart
+++ lib/ui/shared/const_widgets.dart
@@ -6,85 +6,119 @@ class SVGAssetPaths {
   static final dropDownClosedIcon = "assets/icons/drop_down_closed.svg";
 }
 
-class ShortCuts{
-
-  Widget contain({Widget? child,double h = 17, double w = 30}) {
+class ShortCuts {
+  Widget contain({Widget? child, double h = 17, double w = 30}) {
     return Container(
       constraints: BoxConstraints(
-          minWidth: 30,
-          minHeight: 16,
-          maxWidth: w,
-          maxHeight: h
-      ),
+          minWidth: 30, minHeight: 16, maxWidth: w, maxHeight: h),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(4.0),
-          color: Colors.grey.shade300
-      ),
+          color: Colors.grey.shade300),
       child: child,
     );
   }
 
   Widget up() {
     return contain(
-      child: Icon(Icons.arrow_drop_up, size: 20,color: Colors.grey,),
+      child: Icon(
+        Icons.arrow_drop_up,
+        size: 20,
+        color: Colors.black,
+      ),
     );
   }
 
   Widget ctrl() {
     return contain(
-      child: Center(child: Text('Ctrl',style: TextStyle(color: Colors.grey,fontSize: 10),)),
+      child: Center(
+          child: Text(
+        'Ctrl',
+        style: TextStyle(color: Colors.black, fontSize: 10),
+      )),
     );
   }
 
   Widget enter() {
     return contain(
-      child: Center(child: Text('Enter',style: TextStyle(color: Colors.grey,fontSize: 10),)),
+      child: Center(
+          child: Text(
+        'Enter',
+        style: TextStyle(color: Colors.black, fontSize: 10),
+      )),
     );
   }
 
   Widget shift() {
     return contain(
-      child: Center(child: Text('Shift',style: TextStyle(color: Colors.grey,fontSize: 8),)),
+      child: Center(
+          child: Text(
+        'Shift',
+        style: TextStyle(color: Colors.black, fontSize: 8),
+      )),
     );
   }
 
   Widget fKey() {
     return contain(
-      child: Center(child: Text('F',style: TextStyle(color: Colors.grey,fontSize: 10),)),
+      child: Center(
+          child: Text(
+        'F',
+        style: TextStyle(color: Colors.black, fontSize: 10),
+      )),
     );
   }
 
   Widget kKey() {
     return contain(
-      child: Center(child: Text('K',style: TextStyle(color: Colors.grey,fontSize: 10),)),
+      child: Center(
+          child: Text(
+        'K',
+        style: TextStyle(color: Colors.black, fontSize: 10),
+      )),
     );
   }
 
   Widget pageUp() {
     return contain(
       w: 40,
-      child: Center(child: Text('Page up',style: TextStyle(color: Colors.grey,fontSize: 9),)),
+      child: Center(
+          child: Text(
+        'Page up',
+        style: TextStyle(color: Colors.black, fontSize: 9),
+      )),
     );
   }
 
   Widget pageDown() {
     return contain(
       w: 50,
-      child: Center(child: Text('Page Down',style: TextStyle(color: Colors.grey,fontSize: 9),)),
+      child: Center(
+          child: Text(
+        'Page Down',
+        style: TextStyle(color: Colors.black, fontSize: 9),
+      )),
     );
   }
 
   Widget home() {
     return contain(
       w: 40,
-      child: Center(child: Text('Home',style: TextStyle(color: Colors.grey,fontSize: 9),)),
+      child: Center(
+          child: Text(
+        'Home',
+        style: TextStyle(color: Colors.black, fontSize: 9),
+      )),
     );
   }
 
   Widget end() {
     return contain(
       w: 45,
-      child: Center(child: Text('End',style: TextStyle(color: Colors.grey,fontSize: 7),)),
+      child: Center(
+          child: Text(
+        'End',
+        style: TextStyle(color: Colors.black, fontSize: 7),
+      )),
     );
   }
 }
