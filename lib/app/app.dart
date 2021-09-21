be030ecc2e7a948a5e50fb0b3795fb0438836c7a
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2021-09-22 00:42:21 +0100

    Add window title bar service and register it

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,46 +4,33 @@ import 'package:stacked_themes/stacked_themes.dart';
 import 'package:zc_desktop_flutter/services/api/api_service.dart';
 import 'package:zc_desktop_flutter/services/authentication/auth_service.dart';
 import 'package:zc_desktop_flutter/services/local_storage/local_storage_service.dart';
+import 'package:zc_desktop_flutter/services/window_title_bar_service.dart';
 import 'package:zc_desktop_flutter/services/workspace_service/workspace_service.dart';
-import 'package:zc_desktop_flutter/ui/auth/change_password_page/change_password_view.dart';
-import 'package:zc_desktop_flutter/ui/auth/check_email_page/check_email_view.dart';
-import 'package:zc_desktop_flutter/ui/auth/forgot_password_page/forgot_password_view.dart';
-import 'package:zc_desktop_flutter/ui/auth/login_page/login_view.dart';
-import 'package:zc_desktop_flutter/ui/auth/login_page/splash_view.dart';
-import 'package:zc_desktop_flutter/ui/auth/reset_password_page/reset_password_view.dart';
-import 'package:zc_desktop_flutter/ui/auth/sign_up_page/sign_up_view.dart';
-import 'package:zc_desktop_flutter/ui/auth/success_page/success_view.dart';
-import 'package:zc_desktop_flutter/ui/main/channels_page/channels_view.dart';
-import 'package:zc_desktop_flutter/ui/main/home_page/home_view.dart';
-import 'package:zc_desktop_flutter/ui/main/workspace_page/workspace_view.dart';
-import 'package:zc_desktop_flutter/ui/startup_page/startup_view.dart';
-import 'package:zc_desktop_flutter/ui/workspace_registeration_screen/create_workspace.dart';
-import 'package:zc_desktop_flutter/ui/workspace_registeration_screen/create_workspace_stage1.dart';
-import 'package:zc_desktop_flutter/ui/workspace_registeration_screen/create_workspace_stage2.dart';
-import 'package:zc_desktop_flutter/ui/workspace_registeration_screen/create_workspace_stage3.dart';
-import 'package:zc_desktop_flutter/ui/main/channels_display/channels_display_view.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/change_password_page/change_password_view.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/check_email_page/check_email_view.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/forgot_password_page/forgot_password_view.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/login_page/login_view.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/reset_password_page/reset_password_view.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/signUp/signup_view.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/success_page/success_view.dart';
+import 'package:zc_desktop_flutter/ui/views/main/channels_page/channels_view.dart';
+import 'package:zc_desktop_flutter/ui/views/main/home_page/home_view.dart';
+import 'package:zc_desktop_flutter/ui/views/main/workspace_page/workspace_view.dart';
+import 'package:zc_desktop_flutter/ui/views/startup_page/startup_view.dart';
 
 @StackedApp(
   routes: [
-    MaterialRoute(page: HomeView),
-    AdaptiveRoute(page: WorkspaceView),
-    MaterialRoute(
-      page: LoginView,
-    ),
-    MaterialRoute(page: SplashView, initial: true),
-    MaterialRoute(page: StartUpView),
+    MaterialRoute(page: StartUpView, initial: true),
+    MaterialRoute(page: LoginView),
     MaterialRoute(page: SignUpView),
     MaterialRoute(page: ForgotPasswordView),
     MaterialRoute(page: CheckEmailView),
     MaterialRoute(page: ResetPasswordView),
     MaterialRoute(page: ChangePasswordView),
     MaterialRoute(page: SuccessView),
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: WorkspaceView),
     MaterialRoute(page: ChannelsView),
-    MaterialRoute(page: CreateWorkspaceView),
-    MaterialRoute(page: CreateWorkspaceStage1),
-    MaterialRoute(page: CreateWorkspaceStage2),
-    MaterialRoute(page: CreateWorkspaceStage3),
-    MaterialRoute(page: ChannelsDisplayView),
   ],
   dependencies: [
     Presolve(
@@ -55,7 +42,10 @@ import 'package:zc_desktop_flutter/ui/main/channels_display/channels_display_vie
     LazySingleton(classType: ApiService),
     LazySingleton(classType: WorkspaceService),
     LazySingleton(
-        classType: ThemeService, resolveUsing: ThemeService.getInstance)
+      classType: ThemeService,
+      resolveUsing: ThemeService.getInstance,
+    ),
+    LazySingleton(classType: WindowTitleBarService),
   ],
   logger: StackedLogger(),
 )
--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -10,57 +10,42 @@ import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked/stacked_annotations.dart';
 
-import '../ui/auth/change_password_page/change_password_view.dart';
-import '../ui/auth/check_email_page/check_email_view.dart';
-import '../ui/auth/forgot_password_page/forgot_password_view.dart';
-import '../ui/auth/login_page/login_view.dart';
-import '../ui/auth/reset_password_page/reset_password_view.dart';
-import '../ui/auth/sign_up_page/sign_up_view.dart';
-import '../ui/auth/success_page/success_view.dart';
-import '../ui/main/channels_display/channels_display_view.dart';
-import '../ui/main/channels_page/channels_view.dart';
-import '../ui/main/home_page/home_view.dart';
-import '../ui/main/workspace_page/workspace_view.dart';
-import '../ui/startup_page/startup_view.dart';
-import '../ui/workspace_registeration_screen/create_workspace.dart';
-import '../ui/workspace_registeration_screen/create_workspace_stage1.dart';
-import '../ui/workspace_registeration_screen/create_workspace_stage2.dart';
-import '../ui/workspace_registeration_screen/create_workspace_stage3.dart';
+import '../ui/views/auth/change_password_page/change_password_view.dart';
+import '../ui/views/auth/check_email_page/check_email_view.dart';
+import '../ui/views/auth/forgot_password_page/forgot_password_view.dart';
+import '../ui/views/auth/login_page/login_view.dart';
+import '../ui/views/auth/reset_password_page/reset_password_view.dart';
+import '../ui/views/auth/signUp/signup_view.dart';
+import '../ui/views/auth/success_page/success_view.dart';
+import '../ui/views/main/channels_page/channels_view.dart';
+import '../ui/views/main/home_page/home_view.dart';
+import '../ui/views/main/workspace_page/workspace_view.dart';
+import '../ui/views/startup_page/startup_view.dart';
 
 class Routes {
-  static const String homeView = '/home-view';
-  static const String workspaceView = '/workspace-view';
-  static const String loginView = '/';
-  static const String startUpView = '/start-up-view';
+  static const String startUpView = '/';
+  static const String loginView = '/login-view';
   static const String signUpView = '/sign-up-view';
   static const String forgotPasswordView = '/forgot-password-view';
   static const String checkEmailView = '/check-email-view';
   static const String resetPasswordView = '/reset-password-view';
   static const String changePasswordView = '/change-password-view';
   static const String successView = '/success-view';
+  static const String homeView = '/home-view';
+  static const String workspaceView = '/workspace-view';
   static const String channelsView = '/channels-view';
-  static const String createWorkspaceView = '/create-workspace-view';
-  static const String createWorkspaceStage1 = '/create-workspace-stage1';
-  static const String createWorkspaceStage2 = '/create-workspace-stage2';
-  static const String createWorkspaceStage3 = '/create-workspace-stage3';
-  static const String channelsDisplayView = '/channels-display-view';
   static const all = <String>{
-    homeView,
-    workspaceView,
-    loginView,
     startUpView,
+    loginView,
     signUpView,
     forgotPasswordView,
     checkEmailView,
     resetPasswordView,
     changePasswordView,
     successView,
+    homeView,
+    workspaceView,
     channelsView,
-    createWorkspaceView,
-    createWorkspaceStage1,
-    createWorkspaceStage2,
-    createWorkspaceStage3,
-    channelsDisplayView,
   };
 }
 
@@ -68,35 +53,24 @@ class StackedRouter extends RouterBase {
   @override
   List<RouteDef> get routes => _routes;
   final _routes = <RouteDef>[
-    RouteDef(Routes.homeView, page: HomeView),
-    RouteDef(Routes.workspaceView, page: WorkspaceView),
-    RouteDef(Routes.loginView, page: LoginView),
     RouteDef(Routes.startUpView, page: StartUpView),
+    RouteDef(Routes.loginView, page: LoginView),
     RouteDef(Routes.signUpView, page: SignUpView),
     RouteDef(Routes.forgotPasswordView, page: ForgotPasswordView),
     RouteDef(Routes.checkEmailView, page: CheckEmailView),
     RouteDef(Routes.resetPasswordView, page: ResetPasswordView),
     RouteDef(Routes.changePasswordView, page: ChangePasswordView),
     RouteDef(Routes.successView, page: SuccessView),
+    RouteDef(Routes.homeView, page: HomeView),
+    RouteDef(Routes.workspaceView, page: WorkspaceView),
     RouteDef(Routes.channelsView, page: ChannelsView),
-    RouteDef(Routes.createWorkspaceView, page: CreateWorkspaceView),
-    RouteDef(Routes.createWorkspaceStage1, page: CreateWorkspaceStage1),
-    RouteDef(Routes.createWorkspaceStage2, page: CreateWorkspaceStage2),
-    RouteDef(Routes.createWorkspaceStage3, page: CreateWorkspaceStage3),
-    RouteDef(Routes.channelsDisplayView, page: ChannelsDisplayView),
   ];
   @override
   Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
   final _pagesMap = <Type, StackedRouteFactory>{
-    HomeView: (data) {
+    StartUpView: (data) {
       return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => const HomeView(),
-        settings: data,
-      );
-    },
-    WorkspaceView: (data) {
-      return buildAdaptivePageRoute<AdaptiveRoute<dynamic>>(
-        builder: (context) => const WorkspaceView(),
+        builder: (context) => const StartUpView(),
         settings: data,
       );
     },
@@ -106,15 +80,9 @@ class StackedRouter extends RouterBase {
         settings: data,
       );
     },
-    StartUpView: (data) {
-      return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => const StartUpView(),
-        settings: data,
-      );
-    },
     SignUpView: (data) {
       return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => SignUpView(),
+        builder: (context) => const SignUpView(),
         settings: data,
       );
     },
@@ -148,39 +116,21 @@ class StackedRouter extends RouterBase {
         settings: data,
       );
     },
+    HomeView: (data) {
+      return MaterialPageRoute<MaterialRoute<dynamic>>(
+        builder: (context) => const HomeView(),
+        settings: data,
+      );
+    },
+    WorkspaceView: (data) {
+      return MaterialPageRoute<MaterialRoute<dynamic>>(
+        builder: (context) => const WorkspaceView(),
+        settings: data,
+      );
+    },
     ChannelsView: (data) {
       return MaterialPageRoute<MaterialRoute<dynamic>>(
         builder: (context) => ChannelsView(),
-        settings: data,
-      );
-    },
-    CreateWorkspaceView: (data) {
-      return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => const CreateWorkspaceView(),
-        settings: data,
-      );
-    },
-    CreateWorkspaceStage1: (data) {
-      return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => const CreateWorkspaceStage1(),
-        settings: data,
-      );
-    },
-    CreateWorkspaceStage2: (data) {
-      return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => const CreateWorkspaceStage2(),
-        settings: data,
-      );
-    },
-    CreateWorkspaceStage3: (data) {
-      return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => const CreateWorkspaceStage3(),
-        settings: data,
-      );
-    },
-    ChannelsDisplayView: (data) {
-      return MaterialPageRoute<MaterialRoute<dynamic>>(
-        builder: (context) => const ChannelsDisplayView(),
         settings: data,
       );
     },
