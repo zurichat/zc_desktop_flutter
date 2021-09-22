1a1532d7e6714705e0856dedd41fcb38a190130a
Etukudo Emmanuel <etukzbong@gmail.com>
2021-09-22 06:43:18 +0100

    created status dialog minimized edition

---
+++ lib/ui/main/status_dialog/status_dialog_min/status_dialog_min_view.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:stacked/stacked.dart';
+import 'package:zc_desktop_flutter/ui/main/status_dialog/status_dialog_min/status_dialog_min_viewmodel.dart';
+import 'package:zc_desktop_flutter/ui/shared/const_app_colors.dart';
+import 'package:zc_desktop_flutter/ui/shared/const_text_styles.dart';
+import 'package:zc_desktop_flutter/ui/shared/const_ui_helpers.dart';
+import 'package:zc_desktop_flutter/ui/shared/dumb_widgets/zcdesk_text.dart';
+
+class StatusDialogMinView extends StatelessWidget {
+  const StatusDialogMinView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<StatusDialogMinViewModel>.reactive(
+        viewModelBuilder: () => StatusDialogMinViewModel(),
+        builder: (context, model, child) => Dialog(
+              child: dialogContainer(model),
+            ));
+  }
+
+  dialogContainer(StatusDialogMinViewModel model) {
+    return Container(
+      width: 547.h,
+      height: 268.w,
+      padding: EdgeInsets.symmetric(vertical: 24.h, horizontal: 24.w),
+      child: Stack(children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ZcdeskText.headingThree(model.setAStatus),
+                IconButton(onPressed: model.popDialog, icon: Icon(Icons.close))
+              ],
+            ),
+            Container(
+              height: 61.h,
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(),
+                  hintText: model.hintText,
+                  prefixIcon: IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset('assets/icons/emoji_icon.svg')),
+                  suffixIcon:
+                      IconButton(onPressed: () {}, icon: Icon(Icons.clear)),
+                ),
+              ),
+            ),
+            verticalSpaceSmall,
+            Row(
+              children: [
+                ZcdeskText.dropDownBodyTextStyle(model.clearAfter),
+                IconButton(
+                    onPressed: () {}, icon: Icon(Icons.keyboard_arrow_down))
+              ],
+            ),
+            verticalSpaceRegular,
+          ],
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          //child: Padding(
+            //padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    fixedSize: Size(117.w, 48.h),
+                    textStyle: clearStatusTextStyle,
+                    padding: EdgeInsets.symmetric( horizontal: 16.w),
+                    primary: Colors.white,
+                    onPrimary: clearStatusBtnTxtColor,
+                    side: BorderSide(
+                        color: clearStatusBtnBorderColor,
+                        width: 1.0.w),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.r))),
+                child: Text(model.clearBtnText)),
+          //),
+        ),
+      ]),
+    );
+  }
+}
