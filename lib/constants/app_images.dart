4cdbaa4c6dbd27bbcd303e9262729f26dc263760
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2021-09-24 22:22:48 +0100

    Refactor images and strings

--- lib/constants/app_images.dart
+++ lib/constants/app_images.dart
@@ -1,2 +1,24 @@
-const String CircularImage = 'assets/lottie/hng.gif';
-const String AppIcon = 'assets/icons/zuri.svg';
+// TODO: Arrange strings alphabetically
+
+const String CircularImageGif = 'assets/lottie/hng.gif';
+const String AppSvg = 'assets/icons/zuri.svg';
+const String ZuriLogo = 'assets/images/zc_logo2.png';
+const String ZuriSvg = "assets/images/zc_icon.svg";
+const String GoogleLogo = 'assets/images/google.png';
+const String FacebookLogo = 'assets/images/facebook.png';
+const String TwitterLogo = 'assets/images/twitter.png';
+const String ChannelsListSvg = "assets/icons/hash_inactive.svg";
+const String DropDownOpenSvg = "assets/icons/drop_down_open.svg";
+const String DropDownClosedSvg = "assets/icons/drop_down_closed.svg";
+const String ChannelDropDownSvg = "assets/icons/channel_drop_down.svg";
+const String SortIconSvg = "assets/icons/sort.svg";
+const String FilterDownSvg = "assets/icons/filter.svg";
+const String ActiveSvg = "assets/icons/active.svg";
+const String FluentEmojiSvg = "assets/icons/fluent_emoji.svg";
+const String ShareSvg = "assets/icons/share.svg";
+const String ActionsSvg = "assets/icons/actions.svg";
+const String BookmarkSvg = "assets/icons/bookmark.svg";
+const String PinnedSvg = "assets/icons/pinned.svg";
+const String AddSvg = "assets/icons/add.svg";
+const String ThreadSvg = "assets/icons/threads.svg";
+const String NewDmSvg = "assets/icons/new_dm.svg";
