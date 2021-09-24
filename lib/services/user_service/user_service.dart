91563e2a522ae6b94c53dd95211351093358552b
toxic-bishop <okanlawon.daniel@yahoo.com>
2021-09-24 18:59:49 +0100

    commit

--- lib/services/user_service/user_service.dart
+++ lib/services/user_service/user_service.dart
@@ -4,6 +4,7 @@ import 'package:zc_desktop_flutter/models/auth_response.dart';
 import 'package:zc_desktop_flutter/models/user.dart';
 import 'package:zc_desktop_flutter/services/local_storage/local_storage_service.dart';
 
+
 class UserService {
   final _localStorageService = locator<LocalStorageService>();
   User? user;
