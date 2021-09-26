e2363a738f643ade707c522d5b77fcd7c533183c
Isaac Adeni Adariku <isaacadariku05@gmail.com>
2021-09-26 04:11:51 +0100

    refactor success view complete

--- lib/ui/views/auth/success/success_view.dart
+++ lib/ui/views/auth/success/success_view.dart
@@ -3,6 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:stacked/stacked.dart';
 import 'package:zc_desktop_flutter/constants/asset_paths.dart';
+import 'package:zc_desktop_flutter/ui/shared/const_text_styles.dart';
+import 'package:zc_desktop_flutter/ui/shared/const_ui_helpers.dart';
+import 'package:zc_desktop_flutter/ui/shared/dumb_widgets/zcdesk_auth_btn.dart';
 import 'package:zc_desktop_flutter/ui/views/auth/success/success_viewmodel.dart';
 
 class SuccessView extends StatelessWidget {
@@ -14,67 +17,30 @@ class SuccessView extends StatelessWidget {
       viewModelBuilder: () => SuccessViewModel(),
       builder: (BuildContext context, model, Widget? child) {
         return Scaffold(
-          body: Column(
-            children: [
-              Column(
+          body: Container(
+            child: Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SvgPicture.asset(SuccessSvgPath),
-                      Center(
-                        child: SvgPicture.asset(MarkSvgPath),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 32.h),
+                  SvgPicture.asset(SuccessCheckPath),
+                  verticalSpaceMediumTwo,
                   Text(
                     'Email verification successful!',
-                    style: TextStyle(
-                        fontSize: 31.sp,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'Lato',
-                        color: Colors.black),
+                    style: headline3.copyWith(),
                   ),
-                  SizedBox(height: 24.h),
-                  Container(
-                    width: 454.w,
-                    child: Text(
-                      'Click on the Button to continue',
-                      style: TextStyle(
-                          fontSize: 21.sp,
-                          fontWeight: FontWeight.w400,
-                          fontFamily: 'Lato',
-                          color: Colors.black),
-                      textAlign: TextAlign.center,
-                    ),
+                  verticalSpaceMedium,
+                  Text(
+                    'Click on the Button to continue',
+                    style: bodyText1.copyWith(fontSize: 16.sp),
                   ),
-                  SizedBox(
-                    height: 32.h,
-                  ),
-                  GestureDetector(
-                    onTap: model.navigateToLoginView,
-                    child: Container(
-                      height: 48.h,
-                      width: 154.w,
-                      color: Color.fromRGBO(0, 184, 124, 1),
-                      child: Center(
-                        child: Text(
-                          'Continue to Login',
-                          style: TextStyle(
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.w400,
-                              fontFamily: 'Lato',
-                              color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  )
+                  verticalSpaceMediumTwo,
+                  AuthButton(
+                      label: 'Continue to Login',
+                      onTap: model.navigateToLoginView),
                 ],
               ),
-            ],
+            ),
           ),
         );
       },
--- lib/ui/views/auth/success/success_viewmodel.dart
+++ lib/ui/views/auth/success/success_viewmodel.dart
@@ -6,7 +6,7 @@ import 'package:zc_desktop_flutter/app/app.router.dart';
 class SuccessViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
-  void navigateToLoginView() {
-    _navigationService.navigateTo(Routes.loginView);
+  void navigateToLoginView() async {
+    await _navigationService.clearStackAndShow(Routes.loginView);
   }
 }
