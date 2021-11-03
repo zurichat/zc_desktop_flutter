a8f020c557e55da8c00a4ce33d99506222791359
toxic-bishop <okanlawon.daniel@yahoo.com>
2021-11-03 19:34:23 +0000

    login & signup view model test

--- test/viewmodel_tests/login_viewmodel_test.dart
+++ test/viewmodel_tests/login_viewmodel_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:mockito/mockito.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/login/login_viewmodel.dart';
 import '../helpers/test_helpers.dart';
 
 void main() {
@@ -7,9 +8,26 @@ void main() {
   setUp(() => registerServices());
   tearDown(() => unregisterServices());
 
-  group('runLoginLogic -', () {
-   test('', () {
-  
+  group('Login-', () {
+   test('Verify that loginViewModel.login user called from the AuthService with the pass in value', () {
+     final authService = getAndRegisterAuthService();
+     final loginViewModel = LoginViewModel();
+     loginViewModel.login('toxic', 'test');
+     verify(authService.loginUser(email: 'toxic', password: 'test'));
+   });
+
+   test('Verify that when login is called gotoChooseWorkSpace is called to redirect the user to choose workspace view', () async {
+     final loginViewModel = LoginViewModel();
+     await loginViewModel.login('email', 'password');
+     verify(loginViewModel.gotoChooseWorkSpace());
+   });
+
+   group('Verify state update', () { 
+     test('when setPasswordVisibility() is called expect _passwordVisibility to be false', () async {
+       final loginViewModel = LoginViewModel();
+      loginViewModel.setPasswordVisibility();
+      expect(loginViewModel.passwordVisible, false);
+     });
    });
   });
  });
--- test/viewmodel_tests/sign_up_viewmodel_test.dart
+++ test/viewmodel_tests/sign_up_viewmodel_test.dart
@@ -1,15 +1,57 @@
-import 'package:flutter_test/flutter_test.dart';
-
+import 'package:mockito/mockito.dart';
+import 'package:zc_desktop_flutter/ui/views/auth/sign_up/sign_up_viewmodel.dart';
 import '../helpers/test_helpers.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
  group('SignUpViewmodelTest -', (){
   setUp(() => registerServices());
   tearDown(() => unregisterServices());
 
-  group('runLogic -', () {
-   test('', () {
-  
+  group('runLogic setState -', () {
+   test('Verify that when setPasswordVisibility() is called _passwordVisibility is negated', () {
+     final signupViewModel = SignUpViewModel();
+     final passwordVisibility = signupViewModel.passwordVisibility;
+     signupViewModel.setPasswordVisibility();
+     expect(signupViewModel.passwordVisibility, !passwordVisibility);
+   });
+
+   test('Verify that when setConfirmPasswordVisibility() is called _confirmPasswordVisibility is negated', () {
+     final signupViewModel = SignUpViewModel();
+     final confirmPasswordVisibility = signupViewModel.confirmPasswordVisibility;
+     signupViewModel.setPasswordVisibility();
+     expect(signupViewModel.passwordVisibility, !confirmPasswordVisibility);
+   });
+
+   test('Verify that when onPolicyCheckChanged is called _isPolicyChecked is updated to the passed in value', () {
+     final signupViewModel = SignUpViewModel();
+     signupViewModel.onPolicyCheckChanged(true);
+     expect(signupViewModel.isPolicyChecked, true);
+   });
+
+   group('runLogic signup', () {
+     test('verify that when isPolicyChecked is true no error is thrown', () async {
+       final signupViewModel = SignUpViewModel();
+       signupViewModel.onPolicyCheckChanged(true);
+       await signupViewModel.performSignUp('email', 'password', 'Toxic Bishop');
+       expect(await signupViewModel.hasError , false);
+     });
+
+     test('Verify that when perform signup is called authservie sign is called with the fname and lname since there\'s space in the passwed name', () async {
+        final signupViewModel = SignUpViewModel();
+        final authService = getAndRegisterAuthService();
+        signupViewModel.onPolicyCheckChanged(true);
+        await signupViewModel.performSignUp('email ', 'password', 'Toxic Bishop');
+        verifyNever(authService.signup(email: 'email'.trim(), password: 'password', fName:'Toxic', lName: 'Bishop' ));
+     });
+
+     test('throw an error if isPolicyCheckChanged is false ', () async {
+        final signupViewModel = SignUpViewModel();
+        final authService = getAndRegisterAuthService();
+        signupViewModel.onPolicyCheckChanged(true);
+        await signupViewModel.performSignUp('email ', 'password', 'Toxic');
+        verifyNever(authService.signup(email: 'email'.trim(), password: 'password', fName:'Toxic'));
+     });
    });
   });
  });
